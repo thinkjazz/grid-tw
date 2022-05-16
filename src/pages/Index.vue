012c5b6649aae6349b643ac5ea07f562f7a3047f
Bragin Boris <bragin.boris@ya.ru>
2022-05-17 01:19:26 +0300

    Added: terkin wishfuly  backend data structure

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,49 +1,33 @@
 <template>
   <layout class="page-home">
-    <CustomInput
-      placeholder="https://"
-      @change="onInputChange"
-    />
+    <CustomInput placeholder="https://" @change="onInputChange" />
     <div class="space-y-4">
-  <div></div>
-  <div></div>
+      <div></div>
+      <div></div>
+    </div>
 
-</div>
+    <div class="flex justify-center">
+      <div>
+        <CustomButton @click="onButtonClick">Сгененировать</CustomButton>
+      </div>
+    </div>
 
-<div class="flex justify-center">
-  <div>
-      <CustomButton @click="onButtonClick">Сгененировать
-    </CustomButton>
-  </div>
+    <div class="space-y-8">
+      <div>6565</div>
+      <div>656565</div>
+    </div>
 
-</div>
+    <Card />
+    <Divider />
+    <Card />
+    <Divider />
+    <Card />
 
-
-     <div class="space-y-8">
-     <div>6565</div>
-       <div>656565</div>
-     </div>
-
-<Card/>
-<Divider />
-<Card/>
-<Divider />
-<Card/>
-
-
- <button
-      type="button"
-      class="btn"
-      @click="showModal"
-    >
+    <button type="button" class="btn" @click="showModal">
       Open Modal!
     </button>
 
- <Modal
-      v-show="isModalVisible"
-      @close="closeModal"
-    />
-
+    <Modal v-show="isModalVisible" @close="closeModal" />
   </layout>
 </template>
 
@@ -56,14 +40,13 @@ query {
 </page-query>
 
 <script>
-import CustomInput from '../components/Input.vue';
-import CustomButton from '../components/Button.vue';
-import CheckBoxesTree from '../components/CheckBoxesTree.vue';
-import Modal from '../components/Modal.vue';
-import Divider from '../components/Divider.vue';
-import Card from '../components/Card.vue';
-import Log from '../backend/api.js';
-
+import CustomInput from "../components/Input.vue";
+import CustomButton from "../components/Button.vue";
+import CheckBoxesTree from "../components/CheckBoxesTree.vue";
+import Modal from "../components/Modal.vue";
+import Divider from "../components/Divider.vue";
+import Card from "../components/Card.vue";
+import Log from "../backend/api.js";
 
 export default {
   components: {
@@ -76,18 +59,69 @@ export default {
   },
   data() {
     return {
+      uuid: "f9298b1c-cbd5-11ec-9d64-0242ac120002",
+      captcha: "",
+      url: "https://qa.guru",
+      options: {
+        manual_tests: [
+          {
+            title: "Login test",
+            steps: "step1\nstep2",
+          },
+          {
+            title: "Registration test",
+            steps: "step1\nstep2",
+          },
+        ],
+        code: {
+          type: "github",
+          organization: "autotests-cloud",
+          repository_name: "generated",
+          generate_base_tests: true,
+        },
+        infrastructure: {
+          type: "jenkins",
+          create_job: true,
+          launch_job: true,
+        },
+        reporting: {
+          type: "allure",
+          attachments: {
+            screenshots: true,
+            video: true,
+            console_log: true,
+            page_source: true,
+          },
+        },
+        notifications: {
+          type: "telegram",
+          chat: "build_in",
+          bot: "build_in",
+          enabled: true,
+        },
+        test_management_system: {
+          type: "allure_testops",
+          project_name: "generated",
+          enabled: true,
+        },
+        issue_tracker: {
+          type: "jira",
+          project_name: "generated",
+          enabled: true,
+        },
+      },
       formData: {
         url: null,
       },
       isModalVisible: false,
-    };
+    }
   },
   methods: {
     onInputChange(value) {
       this.formData.url = value;
     },
     onButtonClick() {
-      console.log('about to submit form with', this.formData);
+      console.log("about to submit form with", this.formData);
     },
     showModal() {
       this.isModalVisible = true;
@@ -95,7 +129,7 @@ export default {
     closeModal() {
       this.isModalVisible = false;
     },
-  }
+  },
 };
 </script>
 
@@ -103,7 +137,4 @@ export default {
 body {
   background: #f5f5f5;
 }
-
 </style>
-
-
