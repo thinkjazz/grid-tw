ccd33226a9e5740c0946c6669f681541c7928e41
Bragin Boris <bragin.boris@ya.ru>
2022-05-20 01:11:53 +0300

    fix input styles makes bigger font

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -5,11 +5,11 @@
     </label>
     <div class="flex mt-1 rounded-md shadow-sm">
       <span
-        class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 sm:text-sm">
+        class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 lg:text-lg">
         https://
       </span>
       <input type="text" name="autotest" id="autotest"
-        class="flex-1 block w-full min-w-0 px-3 py-2 border-gray-300 rounded-none rounded-r-md focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+        class="flex-1 block w-full min-w-0 px-3 py-2 border-gray-300 rounded-none rounded-r-md focus:ring-yellow-500 focus:border-yellow-500 lg:text-lg"
         v-model="url" :placeholder="url" />
     </div>
   </div>
