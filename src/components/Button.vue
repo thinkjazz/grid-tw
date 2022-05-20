d7e0a89f3b04ca300068b9f6e77414338c4cb283
Bragin Boris <bragin.boris@ya.ru>
2022-05-20 23:32:33 +0300

    Migrate from tailwind 2 => tailwind 3

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,7 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <button type="button" @click.prevent="onButtonClick"
-    class="inline-flex items-center px-6 py-3 text-base font-medium text-white bg-yellow-600 border border-transparent rounded-md shadow-sm hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
+    class="inline-flex items-center px-6 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-amber-500 hover:bg-amber-600 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-amber-600">
     <slot />
   </button>
 </template>
