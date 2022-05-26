bd887396795eddae87af6168b14d2120b6a7b4d5
Bragin Boris <bragin.boris@ya.ru>
2022-05-27 00:17:17 +0300

    Added: TextArea component

---
+++ src/components/TextArea.vue
@@ -0,0 +1,22 @@
+<template>
+   <textarea
+              rows="8"
+              name="manual-test-case"
+
+              class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
+              placeholder="Open 'https://github.com/login'
+
+              Set username 'Alex'
+              Set password '12%#5f'
+              Submit form
+
+              Verify successful authorization as 'Alex'"
+            />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
