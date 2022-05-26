dcffed8774677a122f2242550fc90059eae6201a
Bragin Boris <bragin.boris@ya.ru>
2022-05-27 00:27:31 +0300

    refactor: all logic all logic associated with manual Tests moved to single and index.vue became lightweight

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,7 @@
 <template>
   <layout class="page-home">
     <form v-if="!formDone" @submit.prevent="sendForm">
-      <CustomInput
-        v-model.trim="url"
-        placeholder="https://"
-        @change="onInputChange"
-      />
-
-
+      <InputUrl v-model.trim="url" :placeholder="url" @change="onInputChange" />
 
       <div class="flex justify-center mt-6">
         <div>
@@ -15,19 +9,19 @@
             >Сгененировать</CustomButton
           >
         </div>
-
       </div>
 
       <div class="flex justify-center mt-6 space-y-16">
         <div>
-                          <CustomButton @click="showModal">Open modal</CustomButton>
-                <TailwindModal  v-show="isModalVisible" @close="closeModal"/>
+          <CustomButton @click="showModal">Open modal</CustomButton>
+          <CustomModal v-show="isModalVisible" @close="closeModal" />
         </div>
       </div>
-      <div class="space-y-8">
-        <pre> {{ uuid }}</pre>
-      </div>
-      <div class="flex justify-center mt-6">
+      <div class="space-y-8"></div>
+
+      <ManualTest />
+      <!-- Начало компонента мануал тест-->
+      <!-- <div class="flex justify-center mt-6">
         <div class="form-group">
           <label>Добавить ручной тест</label>
           <CustomButton @click="addTest">ADD</CustomButton>
@@ -40,9 +34,7 @@
           v-for="(manualTest, i) in options.manualTests"
           :key="manualTest.id"
         >
-
           <label @dblclick="removeTest(i)">Manual Test {{ i + 1 }}</label>
-
 
           <input
             v-model.trim="manualTest.title"
@@ -50,18 +42,17 @@
             class="flex-wrap flex-auto block w-full min-w-0 px-3 py-2 mt-2 border-gray-300 rounded-none rounded-r-md focus:ring-yellow-500 focus:border-yellow-500 md:text-md"
           />
 
-
           <textarea
             v-model.trim="manualTest.steps"
             class="flex-wrap flex-auto block w-full min-w-0 px-3 py-2 mt-2 border-gray-300 rounded-none rounded-r-md focus:ring-yellow-500 focus:border-yellow-500 md:text-md"
           ></textarea>
 
           <div class="flex justify-center mt-6">
-           <CustomButton @click="removeTest(i)">DEL</CustomButton>
+            <CustomButton @click="removeTest(i)">DEL</CustomButton>
           </div>
         </div>
-      </div>
-
+      </div> -->
+      <!-- Конец компонента мануал тест -->
       <Card
         :headerIcons="{}"
         :manualTests="[]"
@@ -69,20 +60,13 @@
         :testForm="{ id: 1 }"
       />
       <Divider />
-
-
     </form>
 
-      <div>
-    <!-- <VTailwindModal v-model="show" @confirm="confirm" @cancel="cancel">
-      <template v-slot:title>Hello, vue-final-modal</template>
-      <p>Vue Final Modal is a renderless, stackable, detachable and lightweight modal component.</p>
-    </VTailwindModal> -->
+    <div></div>
 
-
-  </div>
-
-    <div><pre>{{options}}</pre></div>
+    <div>
+      <pre>{{ options }}</pre>
+    </div>
   </layout>
 </template>
 
@@ -95,58 +79,51 @@ query {
 </page-query>
 
 <script>
-import CustomInput from "../components/Input.vue";
+import InputUrl from "../components/InputUrl.vue";
+
 import CustomButton from "../components/Button.vue";
 import CheckBoxesTree from "../components/CheckBoxesTree.vue";
-import Modal from "../components/Modal.vue";
+import ManualTest from "../components/ManualTest.vue";
 import Divider from "../components/Divider.vue";
 import Card from "../components/Card.vue";
 import Log from "../backend/api.js";
-import TailwindModal from "../components/TailwindModal.vue";
-
-
-
-
+import CustomModal from "../components/Modal.vue";
 
 export default {
-
   components: {
-    CustomInput,
+    InputUrl,
     CustomButton,
     CheckBoxesTree,
-    Modal,
     Card,
     Divider,
-    TailwindModal,
-
-
+    CustomModal,
+    ManualTest,
   },
   data() {
     return {
-
-      url: "https://qa.guru",
+      url: "qa.guru",
       uuid: "f9298b1c-cbd5-11ec-9d64-0242ac120002",
       captcha: "",
-      manualTestsAutoIncrement: 0,
+      // manualTestsAutoIncrement: 0,
       formDone: false,
       formReady: false,
       isModalVisible: false,
       options: {
-        manualTests: [
-          {
-            title: "Login test",
-            steps: "step1\nstep2",
-          },
-          {
-            title: "Registration test",
-            steps: "step1\nstep2",
-          },
-          {
-            title: "Console.log test3",
-            steps: "ERROR1\nERROR2",
-          },
-        ],
-          //TODO: подумать как изменить стуктуру данных, чтобы запихать туда labels\описание которое булем рендерить в опциях шаблона
+        // manualTests: [
+        //   {
+        //     title: "Login test",
+        //     steps: "step1\nstep2",
+        //   },
+        //   {
+        //     title: "Registration test",
+        //     steps: "step1\nstep2",
+        //   },
+        //   {
+        //     title: "Console.log test3",
+        //     steps: "ERROR1\nERROR2",
+        //   },
+        // ],
+        //TODO: подумать как изменить стуктуру данных, чтобы запихать туда labels\описание которое булем рендерить в опциях шаблона
         code: {
           type: "github",
           label: "Сгенерировать репозиторий в GitHub",
@@ -194,17 +171,17 @@ export default {
   },
 
   methods: {
-    addTest() {
-      this.options.manualTests.push({
-        id: ++this.manualTestsAutoIncrement,
-        value: "",
-      });
-    },
+    // addTest() {
+    //   this.options.manualTests.push({
+    //     id: ++this.manualTestsAutoIncrement,
+    //     value: "",
+    //   });
+    // },
 
-    removeTest(id) {
-      // this.options.manualTests = this.options.manualTests.filter(item => item.id !== id);
-      this.options.manualTests.splice(id, 1);
-    },
+    // removeTest(id) {
+    //   // this.options.manualTests = this.options.manualTests.filter(item => item.id !== id);
+    //   this.options.manualTests.splice(id, 1);
+    // },
     sendForm() {
       if (this.formReady) {
         this.formDone = true;
@@ -227,9 +204,7 @@ export default {
       this.isModalVisible = false;
     },
   },
-  computed: {
-
-  },
+  computed: {},
 
   mounted() {
     // Log.info("Page mounted");
