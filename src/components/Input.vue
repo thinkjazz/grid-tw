7eff14df7c565939e3af82c301924771d84c05d8
Bragin Boris <bragin.boris@ya.ru>
2022-05-27 00:28:33 +0300

    Added: Simply and reused input component for tc and modal

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,38 +1,9 @@
 <template>
-  <div>
-    <label for="autotest" class="block text-sm font-medium text-gray-700">
-      Бесплатные автотесты для вашего сайта:
-    </label>
-    <div class="flex mt-1 rounded-md shadow-sm">
-      <span
-        class="inline-flex items-center px-3 text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50 lg:text-lg">
-        https://
-      </span>
-      <input type="text" name="autotest" id="autotest"
-        class="flex-1 block w-full min-w-0 px-3 py-2 border-gray-300 rounded-none rounded-r-md focus:ring-yellow-500 focus:border-yellow-500 lg:text-lg"
-        v-model="url" :placeholder="url" />
-    </div>
-  </div>
+  <input
+    type="text"
+    class="flex-wrap flex-auto block w-full min-w-0 px-3 py-2 mt-2 border-gray-300 rounded-md focus:ring-yellow-500 focus:border-yellow-500 md:text-md"
+    placeholder="Введите название теста шаг за шагом"
+  />
 </template>
-
-<script>
-export default {
-  props: {
-    placeholder: {
-      type: String,
-      default: "",
-    }
-
-  },
-  data() {
-    return {
-      url: "qa.guru",
-    };
-  },
-  methods: {
-    onInputChange(event) {
-      this.$emit("change", event.target.value);
-    },
-  },
-};
-</script>
+<script></script>
+<style></style>
